c5ded9181cb3a11afda9c81242f5058a25956d31
Andrej Čižmárik <cizmarik.andrej@gmail.com>
2023-08-11 18:06:29 +0200

    Add .NET Framework processes recognition logic

--- src/Meditation.Core/Services/AttachableProcessListProvider.cs
+++ src/Meditation.Core/Services/AttachableProcessListProvider.cs
@@ -1,15 +1,20 @@
-﻿using Meditation.Common.Models;
+﻿using CliWrap;
+using Meditation.Common.Models;
 using Meditation.Common.Services;
 using Microsoft.Diagnostics.NETCore.Client;
+using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
 
 namespace Meditation.Core.Services
 {
     internal class AttachableProcessListProvider : IAttachableProcessListProvider
     {
         private readonly IProcessListProvider processListProvider;
-        private ImmutableArray<ProcessInfo> attachedProcesses;
+        private ImmutableArray<ProcessInfo> attachableProcesses;
 
         public AttachableProcessListProvider(IProcessListProvider processListProvider)
         {
@@ -18,7 +23,7 @@ namespace Meditation.Core.Services
         }
 
         public ImmutableArray<ProcessInfo> GetAllAttachableProcesses()
-            => attachedProcesses;
+            => attachableProcesses;
 
         public void Refresh()
         {
@@ -28,11 +33,55 @@ namespace Meditation.Core.Services
 
         private void LoadAttachableProcesses()
         {
+            var builder = new List<ProcessInfo>();
+            var netCoreProcesses = LoadNetCoreProcesses();
+            var netCoreProcessesLookup = netCoreProcesses.ToDictionary(p => p.Id, p => p);
+
+            foreach (var netFrameworkProcess in LoadNetFrameworkProcesses())
+            {
+                // NET Framework process obtaining logic might not be 100% reliable, trust .NET Core client primarily
+                if (netCoreProcessesLookup.ContainsKey(netFrameworkProcess.Id))
+                    continue;
+
+                builder.Add(netFrameworkProcess);
+            }
+
+            builder.AddRange(netCoreProcessesLookup.Values);
+            attachableProcesses = builder.ToImmutableArray();
+        }
+
+        private IEnumerable<ProcessInfo> LoadNetFrameworkProcesses()
+        {
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                yield break;
+
+            var stdout = new StringBuilder();
+            Cli.Wrap("cmd")
+                .WithArguments("/c \"tasklist /m \"mscorlib*\" /fo csv /nh\"")
+                .WithStandardOutputPipe(PipeTarget.ToStringBuilder(stdout))
+                .ExecuteAsync()
+                .GetAwaiter()
+                .GetResult();
+
+            using var textReader = new StringReader(stdout.ToString());
+            string? currentLine;
+            while ((currentLine = textReader.ReadLine()) != null)
+            {
+                var tokens = currentLine.Split(',');
+                var rawPid = tokens[1].Trim('\"');
+                if (!int.TryParse(rawPid, out var pid) || !processListProvider.TryGetProcessById(pid, out _))
+                    continue;
+
+                yield return processListProvider.GetProcessById(pid) with { Type = ProcessType.NetFramework };
+            }
+        }
+
+        private IEnumerable<ProcessInfo> LoadNetCoreProcesses()
+        {
             var attachableProcessIds = DiagnosticsClient.GetPublishedProcesses();
-            attachedProcesses = attachableProcessIds
+            return attachableProcessIds
                 .Where(id => processListProvider.TryGetProcessById(id, out _))
-                .Select(pid => processListProvider.GetProcessById(pid) with { Type = ProcessType.NetCoreApp })
-                .ToImmutableArray();
+                .Select(pid => processListProvider.GetProcessById(pid) with { Type = ProcessType.NetCoreApp });
         }
     }
 }
