9530f7b94656a41c07680f953abebe773b16da10
Andrej Čižmárik <cizmarik.andrej@gmail.com>
2023-10-13 15:14:28 +0200

    Apply validations based on review

--- src/Meditation.MetadataLoaderService/Models/MetadataToken.cs
+++ src/Meditation.MetadataLoaderService/Models/MetadataToken.cs
@@ -1,8 +1,65 @@
-﻿namespace Meditation.MetadataLoaderService.Models
+﻿using System;
+
+namespace Meditation.MetadataLoaderService.Models
 {
-    public record struct MetadataToken(int Value);
-    public record struct AssemblyToken(int Value);
-    public record struct ModuleToken(int Value);
-    public record struct TypeDefinitionToken(int Value);
-    public record struct MethodDefinitionToken(int Value);
+    public readonly record struct MetadataToken
+    {
+        public int Value { get; }
+
+        public MetadataToken(int value)
+        {
+            EnsureTokenValueIsValid(value);
+            Value = value;
+        }
+
+        public static void EnsureTokenValueIsValid(int value)
+        {
+            if (value <= 0)
+                throw new ArgumentOutOfRangeException(nameof(value), "Value must be positive.");
+        }
+    }
+
+    public readonly record struct ModuleToken
+    {
+        public int Value { get; }
+
+        public ModuleToken(int value)
+        {
+            MetadataToken.EnsureTokenValueIsValid(value);
+            Value = value;
+        }
+    }
+
+    public readonly record struct AssemblyToken
+    {
+        public int Value { get; }
+
+        public AssemblyToken(int value)
+        {
+            MetadataToken.EnsureTokenValueIsValid(value);
+            Value = value;
+        }
+    }
+
+    public readonly record struct TypeDefinitionToken
+    {
+        public int Value { get; }
+
+        public TypeDefinitionToken(int value)
+        {
+            MetadataToken.EnsureTokenValueIsValid(value);
+            Value = value;
+        }
+    }
+
+    public readonly record struct MethodDefinitionToken
+    {
+        public int Value { get; }
+
+        public MethodDefinitionToken(int value)
+        {
+            MetadataToken.EnsureTokenValueIsValid(value);
+            Value = value;
+        }
+    }
 }
